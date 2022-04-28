45ad70109662f33de0a32388d8222a5a1ead6d81
Valter <valteralvees@outlook.com>
2022-04-28 16:02:15 -0300

    até exercício 12

--- ListaBasic/src/listabasic/ListaBasic.java
+++ ListaBasic/src/listabasic/ListaBasic.java
@@ -8,7 +8,7 @@ public class ListaBasic {
         Scanner teclado = new Scanner(System.in);
         //F = C x 1,8 + 32.
         // 1
-      
+     /*
         System.out.println("Digite a temperatura em Celsius:");
         double celsius = teclado.nextDouble();
         double fahren = celsius * 1.8 +32;
@@ -27,11 +27,27 @@ public class ListaBasic {
         double peso = teclado.nextDouble();
         double imc = peso/(altura*altura);
         System.out.println("O seu IMC é de "+imc);
-        
+       
         //4
+        int escolha =0;
         System.out.println("Digite um valor:");
         double valor = teclado.nextDouble();
-        double quadrado = valor * valor;
+        System.out.println("Por favor, selecione o que você deseja ver:");
+        System.out.println("1- Quadrado do número");
+        System.out.println("2- Cubo do número");
+        escolha = teclado.nextInt();
+        switch(escolha){
+            case 1:
+                double quadrado = valor * valor;
+                System.out.println("O número "+valor +" ao quadrado é "+quadrado);
+            break;
+            case 2:
+                double cubo = valor * valor * valor;
+                System.out.println("O número "+valor +" ao cubo é "+cubo+".");
+            break;
+        }
+       
+        
         double cubo = valor * valor * valor;
         System.out.println("O número "+valor +" ao quadrado é "+quadrado+" e ao cubo é "+cubo+".");
         
@@ -102,9 +118,53 @@ public class ListaBasic {
             } else {
                 System.out.println("O número "+ numPrimo +" não é primo.");
             }
-        
+      
         //10
+        final int ADIVINHA = 34;
+        int contador = 0;
+        System.out.println("Adivinhe o número que estou pensando. Digite um número de 0 a 100.");
+        int numTeste = teclado.nextInt();
+        if (numTeste >0 && numTeste<100){
+            do{
+                if(numTeste > ADIVINHA){
+                    System.out.println("Esse número é MAIOR que o meu...");
+                    contador++;
+                    numTeste = teclado.nextInt();
+                } else if(numTeste < ADIVINHA){
+                    System.out.println("Esse número é MENOR que o meu");
+                    contador++;
+                    numTeste = teclado.nextInt();
+                } else if(numTeste == ADIVINHA){
+                    System.out.println("Você acertou!!!");
+                    contador = contador +10;
+                }
+            }while(contador<10);
+        } else{
+            System.out.println("Digite um número maior que 0 ou menor que 100.");
+        }
         
+        //11
+        System.out.println("Por favor, digite núemros positivos para somar e numeros negativos para parar.");
+        double numUsado = teclado.nextDouble();
+        double somaNumero = 0;
+        while(numUsado >0){
+            somaNumero = somaNumero + numUsado;
+            System.out.println("A soma entre os números é de "+ somaNumero);
+            numUsado = teclado.nextDouble();
+        }
+        System.out.println("O programa foi finalizado");
+        */
+        //12
+        double maior = 0;
+        double numPoxa =0;
+        for(int i=0; i<10; i++){
+            System.out.println("Digite o "+(i+1) +" valor:");
+            numPoxa = teclado.nextDouble();
+            if(numPoxa > maior){
+                maior = numPoxa;
+            }
+        }
+        System.out.println("O maior dos valores é "+maior);
     }
     
 }
